16ef5ab4fccae2b5cca78a75c95d7400e5ae671b
Prasad-201 <prasaddeshpande201@gmail.com>
2023-10-28 20:32:35 +0530

    Second commit in evening

---
+++ resources/views/mailVerification.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>{{$data['title']}}</title>
+</head>
+<body>
+    <h4>OTP Verification</h4>
+    <p>{{ $data['body'] }}</p>
+    <br>
+    <br>
+    <p><b>Thank You!</b></p>
+
+</body>
+</html>
